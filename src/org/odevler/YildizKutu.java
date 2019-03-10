82f85cfe6f5b9f336cae9c012e1ec69d091ed511
olcay1 <nagodreyaclo@gmail.com>
2019-03-10 20:08:10 +0300

    son

--- src/org/odevler/YildizKutu.java
+++ src/org/odevler/YildizKutu.java
@@ -1,11 +1,35 @@
 package org.odevler;
 
+import java.util.Scanner;
+
 public class YildizKutu {
 
 	public static void main(String[] args) {
-		//test
-		
+		// test
+
 		// TODO Auto-generated method stub
+		Scanner klavyedenOku = new Scanner(System.in);
+
+		System.out.print("Satýr giriniz = ");
+		int satir = klavyedenOku.nextInt();
+
+		System.out.print("Sütun giriniz = ");
+		int sutun = klavyedenOku.nextInt();
+
+		for (int satirNumarasi = 1; satirNumarasi <= satir; satirNumarasi++) {
+
+			for (int sutunNumarasi = 1; sutunNumarasi <= sutun; sutunNumarasi++) {
+
+				if (satirNumarasi > 1 && sutunNumarasi > 1 && satirNumarasi < satir && sutunNumarasi < sutun ) {
+					System.out.print("  ");
+					
+				} else {
+					System.out.print("* ");
+				}
+
+			}
+			System.out.println("");
+		}
 
 	}
 
